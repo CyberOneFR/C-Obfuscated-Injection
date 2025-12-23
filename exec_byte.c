2250f2fd3e49682715c60474233ede813d16ddbc
CyberOneFR <erwoan.thebault@gmail.com>
2025-12-23 14:09:10 +0100

    update loop control

--- exec_byte.c
+++ exec_byte.c
@@ -14,24 +14,14 @@
 
 void	function(void)
 {
+	unsigned long	buffer[10];
 	unsigned long	var1;
 	unsigned long	var2;
 
-	var2 = 0x1000000000c3c952;
-	var1 = (&var2)[4];
-	printf("	%p: %p\n", &(&var2)[10], (&var2)[10]);
-	printf("	%p: %p\n", &(&var2)[9], (&var2)[9]);
-	printf("	%p: %p\n", &(&var2)[8], (&var2)[8]);
-	printf("	%p: %p\n", &(&var2)[7], (&var2)[7]);
-	printf("	%p: %p\n", &(&var2)[6], (&var2)[6]);
-	printf("	%p: %p\n", &(&var2)[5], (&var2)[5]);
-	printf("RIP	%p: %p\n", &(&var2)[4], (&var2)[4]);
-	printf("RBP	%p: %p\n", &(&var2)[3], (&var2)[3]);
-	printf("Canary	%p: %p\n", &(&var2)[2], (&var2)[2]);
-	printf("var1	%p: %p\n", &(&var2)[1], (&var2)[1]);
-	printf("var2	%p: %p\n", &(&var2)[0], (&var2)[0]);
-	printf("\n");
-	(&var2)[4] = (unsigned long)function + 25;
+	var2 = 0x1000000000dfeb52;
+	var1 = buffer[13];
+	buffer[13] = (unsigned long)function + 29;
+	printf("woulla\n");
 	var2 = var2 + var1;
 }
 
